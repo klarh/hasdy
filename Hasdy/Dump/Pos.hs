f31eebcb867c1085a95d24a35b5a75a0c3cb9814
Matthew Spellings <mspells@umich.edu>
2013-11-14 11:26:15 -0500

    Updated pos frame format

--- Hasdy/Dump/Pos.hs
+++ Hasdy/Dump/Pos.hs
@@ -6,13 +6,15 @@ import Data.Array.Accelerate as A
 import qualified Data.Text as T
 import Data.Text.Lazy.Builder
 import Data.Text.Format (Format(..), build)
-import Data.Monoid (mconcat)
+import Data.Monoid (mappend, mconcat)
 import Control.Applicative ((<$>))
 
 import Hasdy.Vectors(Vec3'(..))
 
 posFrame::A.Vector (Vec3' Float)->Builder
-posFrame positions = mconcat (posLine <$> A.toList positions)
+posFrame positions = ("def A \"Sphere 1\"\n" `mappend`
+                      mconcat (posLine <$> A.toList positions) `mappend`
+                     "EOF\n")
 
 posLine::Vec3' Float->Builder
 posLine r = build ("{} {} {}\n" :: Format) r
